a8122dc691f3aebbf3f6e4151f80dacf85885596
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-09 11:20:17 +1000

    Added new pages

---
+++ src/pages/blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const blog = () => {
+  return (
+    <div>
+      <h1>This is our blog page</h1>
+    </div>
+  )
+}
+
+export default blog
---
+++ src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const indexPage = () => {
+  return (
+    <div>
+      Hello World!!!
+    </div>
+  )
+}
+
+export default indexPage
