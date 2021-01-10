3b6c93280e45ff70303d116eed1f508950c3569c
Joe Cui <xiaozhou.cui@gmail.com>
2021-01-10 17:00:20 +1000

    Addming product details page template

---
+++ src/pages/products/sectional-sofa.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+const ComponentName = ({ data }) => {
+  return (
+    <div>
+      <h2>{data.product.title}</h2>
+      <h2>{data.product.price}</h2>
+    </div>
+  )
+}
+
+export const query = graphql`
+  {
+    product: contentfulProduct(slug: { eq: "sectional-sofa" }) {
+      title
+      slug
+      price
+    }
+  }
+`
+
+export default ComponentName
---
+++ src/templates/product-template.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// This is the template for all product details pages
+const productTemplate = () => {
+  return (
+    <div>
+      <h1>hello from product template</h1>
+    </div>
+  )
+}
+
+export default productTemplate
